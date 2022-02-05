f300b2cf2185116fd3c24b231ea72466a414ef17
dogmom3 <blair.seivers@yahoo.com>
2022-02-05 13:37:24 -0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// var timeBlocks = ['9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM']
+
+var currentTime = moment();
+moment().format("MMM Do YY");              
+var displayTime = function() {
+    
+    document.getElementById('currentDay').textContent = 
+    currentTime.format('m/d/y, h:m:s');
+}
+
+displayTime();
