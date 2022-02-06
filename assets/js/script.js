5f8d696fb2eb285a36ed75ce8fd98116a0e366af
dogmom3 <blair.seivers@yahoo.com>
2022-02-06 13:07:36 -0500

    no clue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //variable declarations
 var currentDay = document.getElementById('current-day');
 var timeBlock = document.getElementsByClassName('time-block');
-var saveBtn = document.getElementById('saveBtn');
+var saveBtn = document.getElementsByClassName('saveBtn');
 //display todays date
 
 const timeNow = new Date();
@@ -20,13 +20,33 @@ currentDay.textContent = timeNow
 // }
 
 //save button click saves event to local storage
+
+$( ".saveBtn" ).on( "click", notify );
+  function notify() {
+   console.log('clicked')
+  }
+  function onClick(){
+    localStorage.setItem('timeBlock')
+  };
+
+//   saveBtn.addEventListener('click', onClick)
+
+// const button = document.querySelector('saveBtn');
+
+// button.addEventListener('click', event => {
+//   button.textContent = `Click count: ${event.detail}`;
+// });
+
+
 // var saveTasks = function () {
 //     localStorage.setItem("tasks", JSON.stringify(tasks));
 //   };
 
-saveBtn.onclick = function(){};
-   saveBtn.addEventListener('click', function(){
-    localStorage.setItem('timeBlo
+// saveBtn.onclick = function(){};
+//    saveBtn.addEventListener('click', function(){
+//        console.log('clicked')
+
+//     
 
 // var loadTasks = function () {
 //     tasks = JSON.parse(localStorage.getItem("tasks"));
@@ -35,4 +55,5 @@ saveBtn.onclick = function(){};
 
 
 //saved events stay on the page after refresh
+// event.preventDefault();
 
