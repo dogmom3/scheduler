3ee3ca0984bda15b41506a1d7db8caa235031526
dogmom3 <blair.seivers@yahoo.com>
2022-02-07 09:57:13 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,13 @@ const timeNow = new Date();
 console.log(timeNow);
 currentDay.textContent = timeNow
 
+let hours = timeNow.getHours();
+
 //organize time blocks by past present and future
-if (timeBlock < currentDay) {
+if (hours < timeNow) {
     $(this).addClass('past');
 }
-else if (timeBlock === currentDay) {
+else if (hours === currentDay) {
     $(this).addClass('present');
 }
 else {
@@ -22,30 +24,30 @@ else {
     //save button click saves event to local storage
 $('.saveBtn').on('click', function () {
     var saveTask = $(this)
-   console.log('clicked').siblings('.').val().trim();
+   console.log('clicked');
     localStorage.setItem($('this').siblings('.hour').attr('id'), saveTask);
   });
 
   //function to load tasks
   var loadTasks = function () {
     var nine = localStorage.getItem("9");
-    $("#9").siblings(".description").val(nine);
+    $("#9").siblings(".time-block").val(nine);
     var ten = localStorage.getItem("10");
-    $("#10").siblings(".description").val(ten);
+    $("#10").siblings(".time-block").val(ten);
     var eleven = localStorage.getItem("11");
-    $("#11").siblings(".description").val(eleven);
+    $("#11").siblings(".time-block").val(eleven);
     var twelve = localStorage.getItem("12");
-    $("#12").siblings(".description").val(twelve);
+    $("#12").siblings(".time-block").val(twelve);
     var one = localStorage.getItem("1");
-    $("#1").siblings(".description").val(one);
+    $("#1").siblings(".time-block").val(one);
     var two = localStorage.getItem("2");
-    $("#2").siblings(".description").val(two);
+    $("#2").siblings(".time-block").val(two);
     var three = localStorage.getItem("3");
-    $("#3").siblings(".description").val(three);
+    $("#3").siblings(".time-block").val(three);
     var four = localStorage.getItem("4");
-    $("#4").siblings(".description").val(four);
+    $("#4").siblings(".time-block").val(four);
     var five = localStorage.getItem("5");
-    $("#5") .siblings(".description").val(five);
+    $("#5") .siblings(".time-block").val(five);
   };
 
     // tasks = JSON.parse(localStorage.getItem("tasks"));
@@ -62,11 +64,11 @@ $('.saveBtn').on('click', function () {
 //     localStorage.setItem("tasks", JSON.stringify(tasks));
 //   };
 
-saveBtn.onclick = function(){};
+saveBtn.onclick = function(){
    saveBtn.addEventListener('click', function(){
        console.log('clicked')
-
-
+   });
+}
 
 //saved events stay on the page after refresh
 // event.preventDefault();
