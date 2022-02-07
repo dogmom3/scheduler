38a3c295176a33a75e90f130db235692901830c1
dogmom3 <blair.seivers@yahoo.com>
2022-02-06 23:19:36 -0500

    saveBtn loadTasks function

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Work Day Scheduler Starter Code
 
-* AS A
+* AS A student with a job, hobbies, and 3 young dogs
 
-* I AM
+* I AM very busy and struggle to budget my time
 
-* I WANT
+* I WANT to make this scheduler to keep better track of things
 
-* SO THAT
+* SO THAT I can be more efficient with my time.
 
 * IMAGE:
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,34 +2,54 @@
 var currentDay = document.getElementById('current-day');
 var timeBlock = document.getElementsByClassName('time-block');
 var saveBtn = document.getElementsByClassName('saveBtn');
-//display todays date
 
+//display todays date
 const timeNow = new Date();
 console.log(timeNow);
 currentDay.textContent = timeNow
 
 //organize time blocks by past present and future
-// if (timeBlock < currentDay) {
-//     ().addClass('past');
-// }
-// else if (timeBlock === currentDay) {
-//     ().addClass('present');
-// }
-// else {
-//     ().addClass('future');
-// }
+if (timeBlock < currentDay) {
+    $(this).addClass('past');
+}
+else if (timeBlock === currentDay) {
+    $(this).addClass('present');
+}
+else {
+    $(this).addClass('future');
+}
 
-//save button click saves event to local storage
+    //save button click saves event to local storage
+$('.saveBtn').on('click', function () {
+    var saveTask = $(this)
+   console.log('clicked').siblings('.').val().trim();
+    localStorage.setItem($('this').siblings('.hour').attr('id'), saveTask);
+  });
 
-$( ".saveBtn" ).on( "click", notify );
-  function notify() {
-   console.log('clicked')
-  }
-  function onClick(){
-    localStorage.setItem('timeBlock')
+  //function to load tasks
+  var loadTasks = function () {
+    var nine = localStorage.getItem("9");
+    $("#9").siblings(".description").val(nine);
+    var ten = localStorage.getItem("10");
+    $("#10").siblings(".description").val(ten);
+    var eleven = localStorage.getItem("11");
+    $("#11").siblings(".description").val(eleven);
+    var twelve = localStorage.getItem("12");
+    $("#12").siblings(".description").val(twelve);
+    var one = localStorage.getItem("1");
+    $("#1").siblings(".description").val(one);
+    var two = localStorage.getItem("2");
+    $("#2").siblings(".description").val(two);
+    var three = localStorage.getItem("3");
+    $("#3").siblings(".description").val(three);
+    var four = localStorage.getItem("4");
+    $("#4").siblings(".description").val(four);
+    var five = localStorage.getItem("5");
+    $("#5") .siblings(".description").val(five);
   };
 
-//   saveBtn.addEventListener('click', onClick)
+    // tasks = JSON.parse(localStorage.getItem("tasks"));
+    //   saveBtn.addEventListener('click', onClick)
 
 // const button = document.querySelector('saveBtn');
 
@@ -42,18 +62,13 @@ $( ".saveBtn" ).on( "click", notify );
 //     localStorage.setItem("tasks", JSON.stringify(tasks));
 //   };
 
-// saveBtn.onclick = function(){};
-//    saveBtn.addEventListener('click', function(){
-//        console.log('clicked')
-
-//     
-
-// var loadTasks = function () {
-//     tasks = JSON.parse(localStorage.getItem("tasks"));
-
+saveBtn.onclick = function(){};
+   saveBtn.addEventListener('click', function(){
+       console.log('clicked')
 
 
 
 //saved events stay on the page after refresh
 // event.preventDefault();
 
+loadTasks();
