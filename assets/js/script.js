6976fab1bf8ba173f1d7ca3f478910dd4870ee88
dogmom3 <blair.seivers@yahoo.com>
2022-02-07 15:00:41 -0500

    save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,69 +10,33 @@ currentDay.textContent = timeNow
 
 let hours = timeNow.getHours();
 
-//organize time blocks by past present and future
-if (hours < timeNow) {
-    $(this).addClass('past');
-}
-else if (hours === currentDay) {
-    $(this).addClass('present');
-}
-else {
-    $(this).addClass('future');
-}
 
-    //save button click saves event to local storage
-$('.saveBtn').on('click', function () {
-    var saveTask = $(this)
-   console.log('clicked');
-    localStorage.setItem($('this').siblings('.hour').attr('id'), saveTask);
-  });
+//function to load tasks
+$('textarea').each(function () {
 
-  saveBtn.onclick = (){
-    saveBtn.addEventListener('click', function(){
-        e.event.preventDefault();
-        localStorage.setItem()
-        console.log('clicked')
-    });
-
-  //function to load tasks
-  var loadTasks = function () {
-      
-    var nine = localStorage.getItem("9");
-    
-    var ten = localStorage.getItem("10");
-  
-    var eleven = localStorage.getItem("11");
-   
-    var twelve = localStorage.getItem("12");
-   
-    var one = localStorage.getItem("1");
-   
-    var two = localStorage.getItem("2");
-
-    var three = localStorage.getItem("3");
-
-    var four = localStorage.getItem("4");
-
-    var five = localStorage.getItem("5");
-    
-
-    // tasks = JSON.parse(localStorage.getItem("tasks"));
-    //   saveBtn.addEventListener('click', onClick)
-
-// const button = document.querySelector('saveBtn');
-
-// button.addEventListener('click', event => {
-//   button.textContent = `Click count: ${event.detail}`;
-// });
+    if (hours < timeNow) {
+        $('textarea').addClass('past');
+        $('textarea').removeClass('present');
+        $('textarea').removeClass('future');
+    }
+    else if (hours === timeNow) {
+        $('textarea').addClass('present');
+        $('textarea').removeClass('past');
+        $('textarea').removeClass('future');
+    }
+    else {
+        $('textarea').addClass('future');
+        $('textarea').removeClass('past');
+        $('textarea').removeClass('present');
+    }
+    var loadTask = localStorage.getItem($(this).siblings('h2').attr('id'))
+    $(this).val(loadTask);
+})
 
 
-// var saveTasks = function () {
-//     localStorage.setItem("tasks", JSON.stringify(tasks));
-//   };
+//save button click saves event to local storage
+$('.saveBtn').click(function () {
+    var saveTask = localStorage.setItem($(this).siblings('h2').attr('id'), $(this).siblings('.description').val());
+    console.log('clicked');
+});
 
-
-}
-
-saveBtn.onclick();
-loadTasks();
